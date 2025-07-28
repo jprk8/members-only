1d7a5629296f14c4086b9522c1c870163a433aa9
John Park <176120986+jprk8@users.noreply.github.com>
2025-07-28 11:52:19 -0700

    Modify pool to connect to Railway

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DB,
-    password: process.env.PW,
-    port: 5432
+    connectionString: process.env.DATABASE_URL
 });
